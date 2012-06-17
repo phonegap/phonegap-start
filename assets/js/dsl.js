c9a7a86afab39b21902f4382b305bc053b8ca5b5
Tania-Zaman <taniazaman18@gmail.com>
2012-06-17 14:59:26 -0500

    Update master

--- assets/js/dsl.js
+++ assets/js/dsl.js
@@ -15,7 +15,6 @@ var run = function(application) {
 // shows id passed
 , display = function(id) {
     x$(["#welcome", "#map", "#settings","#video"]).each(function(e, i) {
-    	console.log(e);
         var display = '#' + x$(e)[0].id === id ? 'block' : 'none';
         x$(e).css({ 'display':display })
     });
