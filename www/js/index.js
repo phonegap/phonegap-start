c72ac7ea2b0671c9a67ca7d99367c9cd0432913e
laghriss <lgrs.job@gmail.com>
2012-12-17 13:55:17 +0000

    Update www/js/index.js

--- www/js/index.js
+++ www/js/index.js
@@ -40,3 +40,16 @@ var app = {
         completeElem.className = completeElem.className.split('hide').join('');
     }
 };
+
+function draw() {
+  var canvas = document.getElementById('canvas');
+  if (canvas.getContext) {
+    var ctx = canvas.getContext("2d");
+
+    ctx.fillStyle = "rgb(200,0,0)";
+    ctx.fillRect (10, 10, 55, 50);
+
+    ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+    ctx.fillRect (30, 30, 55, 50);
+  }
+}
