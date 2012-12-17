9f98bb3ea83614ad31958632d5483e2785cb04c2
laghriss <lgrs.job@gmail.com>
2012-12-17 14:53:54 +0000

    Update www/js/index.js

--- www/js/index.js
+++ www/js/index.js
@@ -41,16 +41,102 @@ var app = {
     }
 };
 
-function draw() {
-  var canvas = document.getElementById('canvas');
-  if (canvas.getContext) {
-    var ctx = canvas.getContext("2d");
-
-    ctx.fillStyle = "rgb(200,0,0)";
-    ctx.fillRect (10, 10, 55, 50);
-
-    ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-    ctx.fillRect (30, 30, 55, 50);
-  }
+function init() {
+  clock();
+  setInterval(clock, 1000);
 }
-draw()
+function clock() {
+  var now = new Date();
+  var ctx = document.getElementById('canvas').getContext('2d');
+  ctx.save();
+  ctx.clearRect(0, 0, 150, 150);
+  ctx.translate(75, 75);
+  ctx.scale(0.4, 0.4);
+  ctx.rotate(-Math.PI/2);
+  ctx.strokeStyle = "black";
+  ctx.fillStyle = "white";
+  ctx.lineWidth = 8;
+  ctx.lineCap = "round";
+
+  // Marques des heures
+  ctx.save();
+  ctx.beginPath();
+  for (i = 0; i < 12; i++) {
+    ctx.rotate(Math.PI/6);
+    ctx.moveTo(100, 0);
+    ctx.lineTo(120, 0);
+  }
+  ctx.stroke();
+  ctx.restore();
+
+  // Marques des minutes
+  ctx.save();
+  ctx.lineWidth = 5;
+  ctx.beginPath();
+  for (i = 0; i < 60; i++) {
+    if (i%5 != 0) {
+      ctx.moveTo(117, 0);
+      ctx.lineTo(120, 0);
+    }
+    ctx.rotate(Math.PI/30);
+  }
+  ctx.stroke();
+  ctx.restore();
+  
+  var sec = now.getSeconds();
+  var min = now.getMinutes();
+  var hr  = now.getHours();
+  hr = (hr >= 12) ? hr-12 : hr;
+
+  ctx.fillStyle = "black";
+
+  // Dessin des heures
+  ctx.save();
+  ctx.rotate( hr*(Math.PI/6) + (Math.PI/360)*min + (Math.PI/21600)*sec )
+  ctx.lineWidth = 14;
+  ctx.beginPath();
+  ctx.moveTo(-20, 0);
+  ctx.lineTo(80, 0);
+  ctx.stroke();
+  ctx.restore();
+
+  // Dessin des minutes
+  ctx.save();
+  ctx.rotate( (Math.PI/30)*min + (Math.PI/1800)*sec )
+  ctx.lineWidth = 10;
+  ctx.beginPath();
+  ctx.moveTo(-28, 0);
+  ctx.lineTo(112, 0);
+  ctx.stroke();
+  ctx.restore();
+  
+  // Dessin des secondes
+  ctx.save();
+  ctx.rotate(sec * Math.PI/30);
+  ctx.strokeStyle = "#D40000";
+  ctx.fillStyle = "#D40000";
+  ctx.lineWidth = 6;
+  ctx.beginPath();
+  ctx.moveTo(-30, 0);
+  ctx.lineTo(83, 0);
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.arc(0, 0, 10, 0, Math.PI*2, true);
+  ctx.fill();
+  ctx.beginPath();
+  ctx.arc(95, 0, 10, 0, Math.PI*2, true);
+  ctx.stroke();
+  ctx.fillStyle = "#555";
+  ctx.arc(0, 0, 3, 0, Math.PI*2, true);
+  ctx.fill();
+  ctx.restore();
+
+  ctx.beginPath();
+  ctx.lineWidth = 14;
+  ctx.strokeStyle = '#325FA2';
+  ctx.arc(0, 0, 142, 0, Math.PI*2, true);
+  ctx.stroke();
+
+  ctx.restore();
+}
+init()
