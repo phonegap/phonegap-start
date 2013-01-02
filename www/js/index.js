6dcd155ae67f95af442c675834fe9a6a07dffbde
ali laghriss <lgrs.job@gmail.com>
2013-01-02 14:15:02 +0000

    drag test featur

--- www/js/index.js
+++ www/js/index.js
@@ -40,48 +40,16 @@ var app = {
         completeElem.className = completeElem.className.split('hide').join('');
     }
 };
-var sun = new Image();
-var moon = new Image();
-var earth = new Image();
-function init() {
-  sun.src = 'img/logo.png';
-  moon.src = 'img/logo.png';
-  earth.src = 'img/logo.png';
-  setInterval(draw,100);
-}
 
-function draw() {
-  var ctx = document.getElementById('canvas').getContext('2d');
 
-  ctx.globalCompositeOperation = 'destination-over';
-  ctx.clearRect(0, 0, 300, 300); // efface le canevas
-
-  
-  ctx.fillStyle = 'rgba(0,0,0,0.4)';
-  ctx.strokeStyle = 'rgba(0,153,255,0.4)';
-  ctx.save();
-  ctx.translate(150, 150);
-
-  // Terre
-  var time = new Date();
-  ctx.rotate( ((2*Math.PI)/60)*time.getSeconds() + ((2*Math.PI)/60000)*time.getMilliseconds() );
-  ctx.translate(105,0);
-  ctx.fillRect(0, -12, 50, 24); // Ombre
-  ctx.drawImage(earth, -12, -12);
-
-  // Lune
-  ctx.save();
-  ctx.rotate( ((2*Math.PI)/6)*time.getSeconds() + ((2*Math.PI)/6000)*time.getMilliseconds() );
-  ctx.translate(0, 28.5);
-  ctx.drawImage(moon, -3.5, -3.5);
-  ctx.restore();
-
-  ctx.restore();
-  
-  ctx.beginPath();
-  ctx.arc(150, 150, 105, 0, Math.PI*2, false); // orbite terrestre
-  ctx.stroke();
- 
-  ctx.drawImage(sun, 0, 0, 300, 300);
-}
-init()
+$(function() {
+	$( "#draggable" ).draggable();
+	$( "#droppable" ).droppable({
+		drop: function( event, ui ) {
+			$( this )
+				.addClass( "ui-state-highlight" )
+				.find( "p" )
+					.html( "Dropped!" );
+		}
+	});
+});
