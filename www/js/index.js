a70f36b517b906e1d5ddd22e45961237930be24f
solojavier <1.27201@gmail.com>
2013-04-16 19:39:14 -0500

    Implementing basic retrieve of user data

--- www/js/index.js
+++ www/js/index.js
@@ -1,18 +1,39 @@
-var app = {
-    initialize: function() {
-        this.bind();
-    },
-    bind: function() {
-        document.addEventListener('deviceready', this.deviceready, false);
-    },
-    deviceready: function() {
-        app.report('deviceready');
-    },
-    report: function(id) {
-        console.log("Report: " + id);
+var storage = window.localStorage;
 
-        document.querySelector('#' + id + ' .pending').className += ' hide';
-        var completeElem = document.querySelector('#' + id + ' .complete');
-        completeElem.className = completeElem.className.split('hide').join('');
-    }
-};
+$( document ).delegate("#login-page", "pageinit", function() {
+  $( "#login" ).on( "click", function( event ){
+    event.preventDefault();
+
+    $.ajax({
+      url:'http://mindsweeper.herokuapp.com/api/login',
+      type: "POST",
+      data: {username: $( "#username" ).val(), password: $( "#password" ).val()},
+      dataType: "json",
+      success: function(data, options) {
+        for ( property in data._links ) {
+          storage.setItem(property, data._links[property].href);
+        }
+
+        var ideas = data._embedded.ideas;
+        for (var i = 0; i < ideas.length; i++){
+          idea_name = 'idea['+i+']';
+          idea      = ideas[i];
+          console.log(idea);
+          storage.setItem(idea_name, idea.description);
+
+          for ( property in idea._links ) {
+            storage.setItem(idea_name + '.' + property, idea._links[property].href);
+          }
+        }
+        $.mobile.changePage($("#user-page"));
+      },
+      error: function() {
+        alert("Error");
+      }
+    });
+  });
+});
+
+$( document ).delegate("#user-page", "pageinit", function() {
+  $('#idea').text(storage.getItem('idea[0]'));
+});
