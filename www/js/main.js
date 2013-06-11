552353ee46e782fd2061dffbea6b0c7fd4e5adf1
Roy Lin <roy@luna.co.nz>
2013-06-12 00:44:56 +1200

    first commit

--- www/js/main.js
+++ www/js/main.js
@@ -1,5 +1,13 @@
 var app = {
 
+	showAlert: function (message, title) {
+		if (navigator.notification) {
+			navigator.notification.alert(message, null, title, 'OK');
+		} else {
+			alert(title ? (title + ": " + message) : message);
+		}
+	},
+
     findByName: function() {
         console.log('findByName');
         this.store.findByName($('.search-key').val(), function(employees) {
@@ -14,10 +22,12 @@ var app = {
     },
 
     initialize: function() {
-        this.store = new MemoryStore();
+		var self = this;
+        this.store = new LocalStorageStore(function () {
+			self.showAlert('Store Initialised', 'info');
+		});
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
-
 };
 
 app.initialize();
