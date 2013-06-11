23e4803f4fa5c672ef15344ee2f45c87bf20db21
Roy Lin <roy@luna.co.nz>
2013-06-12 01:13:01 +1200

    something

--- www/js/main.js
+++ www/js/main.js
@@ -8,39 +8,32 @@ var app = {
 		}
 	},
 	
+
 	renderHomeView: function() {
-		var html =
-				"<div class='header'><h1>Home</h1></div>" +
-				"<div class='search-view'>" +
-				"<input class='search-key'/>" +
-				"<ul class='employee-list'></ul>" +
-				"</div>"
-		$('body').html(html);
+		$('body').html(this.homeTpl());
 		$('.search-key').on('keyup', $.proxy(this.findByName, this));
 	},
+		
 	
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    },
+	findByName: function() {
+		var self = this;
+		this.store.findByName($('.search-key').val(), function(employees) {
+			$('.employee-list').html(self.employeeLiTpl(employees));
+		});
+	},
 
     initialize: function() {
 		var self = this;
         this.store = new LocalStorageStore(function () {
 			self.showAlert('Store Initialised', 'info');
 		});
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
 		
 		this.homeTpl = Handlebars.compile($("#home-tpl").html());
-		this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
+		this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());		
+		
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+		
+
     }
 };
 
