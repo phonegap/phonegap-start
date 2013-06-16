10bca1b6c6fb88d1cf972afdba542c0cff6fd4c4
Roy Lin <roy@luna.co.nz>
2013-06-16 13:51:50 +1200

    asdf

--- www/js/main.js
+++ www/js/main.js
@@ -1,5 +1,20 @@
 var app = {
     initialize: function() {
+		navigator.notification.alert("HI!", null, "sup", 'OK');
+	
+	/*
+			
+			$.ajax({
+				url:"views", // relative path to www folder
+				type:"get",
+				contentType:"application/text",
+				context:this,
+				success: function(text){
+					globals.nativeAlert(text);
+				}
+			});
+*/
+	
         this.store = new MemoryStore();
 		new LoginView(this.store);
     }
