500243b55a3a237a0da7290fff2803c17f261c52
Roy Lin <roy@luna.co.nz>
2013-06-16 14:49:27 +1200

    asdf

--- www/js/main.js
+++ www/js/main.js
@@ -1,23 +1,7 @@
 var app = {
     initialize: function() {
-
-	
-
-			
-		$.ajax({
-			url:"views/login.html", // relative path to www folder
-			type:"get",
-			contentType:"application/text",
-			context:this,
-			success: function(text){
-				globals.nativeAlert(text);
-			}
-		});
-		/*
-		
         this.store = new MemoryStore();
 		new LoginView(this.store);
-		*/
     }
 	
 	
