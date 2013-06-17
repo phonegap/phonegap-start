6c4e5664a0e0e262bf297d36f0b39a905826a6e6
Vianna Bao <viannabao@gmail.com>
2013-06-17 16:12:11 +1200

    Added a draggable egg on the GroupingGameView

--- www/core/views/GroupingGameView.js
+++ www/core/views/GroupingGameView.js
@@ -17,5 +17,39 @@ GroupingGameView.initialize = function (employee) {
 	Controller.routeAnchor(".grouping_back", "LoginView", App.store);
 	
 	
+	var imageObj = new Image();
+    imageObj.onload = function() {
+	    drawImage(this);
+   	};
+    imageObj.src = 'images/egg1.png';
+	
 }
 
+function drawImage(imageObj) { 
+
+        var stage = new Kinetic.Stage({
+          container: "container",
+          width: 2048,
+          height: 1000
+        });
+        var layer = new Kinetic.Layer();
+        // darth vader
+        var darthVaderImg = new Kinetic.Image({
+          image: imageObj,
+          x: stage.getWidth() / 2 - 200 / 2,
+          y: stage.getHeight() / 2 - 137 / 2,
+          width: 150,
+          height: 150,
+          draggable: true
+        });
+        // add cursor styling
+        darthVaderImg.on('mouseover', function() {
+          document.body.style.cursor = 'pointer';
+        });
+        darthVaderImg.on('mouseout', function() {
+          document.body.style.cursor = 'default';
+        });
+
+        layer.add(darthVaderImg);
+        stage.add(layer);
+      }
