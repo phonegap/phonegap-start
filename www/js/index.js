f8135980a50c1e448fe3b9322fbf48572a9f3c64
johmcg64 <johmcg64@gmail.com>
2013-10-19 07:07:42 -0600

    Update index.js
    I added the function to get the geo location. It should display it right after the device is ready message in a dialog box.

--- www/js/index.js
+++ www/js/index.js
@@ -43,8 +43,34 @@ var app = {
 
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
+        
+        getGeoLocation()
+
 
         console.log('Received Event: ' + id);
+    },
+    
+    function getGeoLocation() {
+        
+        var onSuccess = function(position) {
+          alert('Latitude: '          + position.coords.latitude          + '\n' +
+                'Longitude: '         + position.coords.longitude         + '\n' +
+                'Altitude: '          + position.coords.altitude          + '\n' +
+                'Accuracy: '          + position.coords.accuracy          + '\n' +
+                'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+                'Heading: '           + position.coords.heading           + '\n' +
+                'Speed: '             + position.coords.speed             + '\n' +
+                'Timestamp: '         + position.timestamp                + '\n');
+        };
+
+        // onError Callback receives a PositionError object
+        //
+        function onError(error) {
+             alert('code: '    + error.code    + '\n' +
+                   'message: ' + error.message + '\n');
+        }
+
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
     }
 };
 
