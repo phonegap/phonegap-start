335a9a003c777865b4551601dd3b25a8811ba328
Max Konev <max@arello-mobile.com>
2014-05-13 21:18:10 -0700

    updated sdk and interface. read our updated guides on http://www.pushwoosh.com

---
+++ www/js/PushwooshAndroid.js
@@ -0,0 +1,160 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+function registerPushwooshAndroid() {
+
+ 	var pushNotification = window.plugins.pushNotification;
+
+	//set push notifications handler
+	document.addEventListener('push-notification',
+		function(event)
+		{
+            var title = event.notification.title;
+            var userData = event.notification.userdata;
+
+            //dump custom data to the console if it exists
+            if(typeof(userData) != "undefined") {
+				console.warn('user data: ' + JSON.stringify(userData));
+			}
+
+			//and show alert
+			navigator.notification.alert(title);
+
+			//stopping geopushes
+			pushNotification.stopGeoPushes();
+		}
+	);
+
+	//initialize Pushwoosh with projectid: "GOOGLE_PROJECT_ID", appid : "PUSHWOOSH_APP_ID". This will trigger all pending push notifications on start.
+	pushNotification.onDeviceReady({ projectid: "60756016005", appid : "539E9-AB8AE" });
+
+	pushNotification.registerDevice(
+		function(token)
+		{
+			alert(token);
+			//callback when pushwoosh is ready
+			onPushwooshAndroidInitialized(token);
+		},
+		function(status)
+		{
+			alert("failed to register: " +  status);
+		    console.warn(JSON.stringify(['failed to register ', status]));
+		}
+	);
+}
+
+function onPushwooshAndroidInitialized(pushToken)
+{
+	//output the token to the console
+	console.warn('push token: ' + pushToken);
+
+	var pushNotification = window.plugins.pushNotification;
+	
+	//if you need push token at a later time you can always get it from Pushwoosh plugin
+	pushNotification.getPushToken(
+		function(token)
+		{
+			console.warn('push token: ' + token);
+		}
+	);
+
+	//and HWID if you want to communicate with Pushwoosh API
+	pushNotification.getPushwooshHWID(
+		function(token) {
+			console.warn('Pushwoosh HWID: ' + token);
+		}
+	);
+	
+	pushNotification.getTags(
+		function(tags)
+		{
+			console.warn('tags for the device: ' + JSON.stringify(tags));
+		},
+		function(error)
+		{
+			console.warn('get tags error: ' + JSON.stringify(error));
+		}
+	);
+	 
+
+	//set multi notificaiton mode
+	//pushNotification.setMultiNotificationMode();
+	//pushNotification.setEnableLED(true);
+	
+	//set single notification mode
+	//pushNotification.setSingleNotificationMode();
+	
+	//disable sound and vibration
+	//pushNotification.setSoundType(1);
+	//pushNotification.setVibrateType(1);
+	
+	pushNotification.setLightScreenOnNotification(false);
+	
+	//goal with count
+	//pushNotification.sendGoalAchieved({goal:'purchase', count:3});
+	
+	//goal with no count
+	//pushNotification.sendGoalAchieved({goal:'registration'});
+
+	//setting list tags
+	//pushNotification.setTags({"MyTag":["hello", "world"]});
+	
+	//settings tags
+	pushNotification.setTags({deviceName:"hello", deviceId:10},
+		function(status) {
+			console.warn('setTags success');
+		},
+		function(status) {
+			console.warn('setTags failed');
+		}
+	);
+		
+	function geolocationSuccess(position) {
+		pushNotification.sendLocation({lat:position.coords.latitude, lon:position.coords.longitude},
+			function(status) {
+				console.warn('sendLocation success');
+			},
+			function(status) {
+				console.warn('sendLocation failed');
+			}
+		);
+	};
+		
+	// onError Callback receives a PositionError object
+	//
+	function geolocationError(error) {
+		alert('code: '    + error.code    + '\n' + 'message: ' + error.message + '\n');
+	}
+	
+	function getCurrentPosition() {
+		navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationError);
+	}
+	
+	//greedy method to get user position every 3 second. works well for demo.
+//	setInterval(getCurrentPosition, 3000);
+		
+	//this method just gives the position once
+//	navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationError);
+		
+	//this method should track the user position as per Phonegap docs.
+//	navigator.geolocation.watchPosition(geolocationSuccess, geolocationError, { maximumAge: 3000, enableHighAccuracy: true });
+
+	//Pushwoosh Android specific method that cares for the battery
+	pushNotification.startGeoPushes();
+}
---
+++ www/js/PushwooshiOS.js
@@ -0,0 +1,98 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+function registerPushwooshIOS() {
+ 	var pushNotification = window.plugins.pushNotification;
+
+ 	//set push notification callback before we initialize the plugin
+	document.addEventListener('push-notification',
+		function(event)
+		{
+			//get the notification payload
+			var notification = event.notification;
+
+			//display alert to the user for example
+			navigator.notification.alert(notification.aps.alert);
+			
+			//to view full push payload
+			//navigator.notification.alert(JSON.stringify(notification));
+			
+			//clear the app badge
+			pushNotification.setApplicationIconBadgeNumber(0);
+		}
+	);
+
+	//initialize the plugin
+    pushNotification.onDeviceReady({pw_appid:"539E9-AB8AE"});
+
+	//register for pushes
+	pushNotification.registerDevice(
+		function(status)
+		{
+			var deviceToken = status['deviceToken'];
+			console.warn('registerDevice: ' + deviceToken);
+			onPushwooshiOSInitialized(deviceToken);
+		},
+		function(status)
+		{
+			console.warn('failed to register : ' + JSON.stringify(status));
+			navigator.notification.alert(JSON.stringify(['failed to register ', status]));
+		}
+	);
+	
+	//reset badges on start
+	pushNotification.setApplicationIconBadgeNumber(0);
+}
+
+function onPushwooshiOSInitialized(pushToken)
+{
+	var pushNotification = window.plugins.pushNotification;
+	//retrieve the tags for the device
+	pushNotification.getTags(
+		function(tags) {
+			console.warn('tags for the device: ' + JSON.stringify(tags));
+		},
+		function(error) {
+			console.warn('get tags error: ' + JSON.stringify(error));
+		}
+	);
+
+	//example how to get push token at a later time 
+	pushNotification.getPushToken(
+		function(token)
+		{
+			console.warn('push token device: ' + token);
+		}
+	);
+
+	//example how to get Pushwoosh HWID to communicate with Pushwoosh API
+	pushNotification.getPushwooshHWID(
+		function(token) {
+			console.warn('Pushwoosh HWID: ' + token);
+		}
+	);
+
+	//start geo tracking.
+	pushNotification.startLocationTracking(
+		function()
+		{
+			   console.warn('Location Tracking Started');
+		}
+	);
+}
